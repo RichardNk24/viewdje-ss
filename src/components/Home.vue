32577e464f02544134c42ebe8defcafc08121d18
RichardNk24 <richardsobelnkulu@gmail.com>
2022-06-23 14:54:08 +0200

    Adding component & modify App content

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template></template>
+
+<script>
+    export default{
+        name: "Home",
+
+        data() {
+            return {
+
+            }
+        },
+
+        methods: {
+            
+        },
+
+        computed: {
+
+        }
+    }
+</script>
+
+<style></style>
