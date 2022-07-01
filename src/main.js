1c5c91f75c1c76a170ff936acbefb7d1be0f5540
RichardNk24 <richardsobelnkulu@gmail.com>
2022-07-01 15:23:39 +0200

    Updating main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,6 @@ import router from "./router";
 import Products from "./components/Products.vue";
 
 const app = createApp(App);
-
 // eslint-disable-next-line vue/multi-word-component-names
 app.component("Products", Products);
 app.use(createPinia());
